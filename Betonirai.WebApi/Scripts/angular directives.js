fda4a725e52a89c68a6784b4f8413ac8f4d49aa0
Ivan <me@ivivanov.com>
2014-08-07 18:19:38 +0300

    some temp code

---
+++ Betonirai.WebApi/Scripts/angular directives.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+ngApp.directive('recaptcha', function () {
+
+    function link(scope, element, attrs) {
+
+        debugger
+        Recaptcha.create(scope.privateKey, attrs.id,
+           {
+               theme: "red",
+               callback: Recaptcha.focus_response_field
+           }
+         );
+    }
+
+    return {
+        restrict: 'A',
+        transclude: true,
+        link: link
+    }
+})
