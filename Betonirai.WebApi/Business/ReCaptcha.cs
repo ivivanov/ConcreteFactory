ca43d4128b224f9d0f2980f05c62d007af17e92b
Ivan <me@ivivanov.com>
2014-08-15 12:20:12 +0300

    validate method

--- Betonirai.WebApi/Business/ReCaptcha.cs
+++ Betonirai.WebApi/Business/ReCaptcha.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Net.Http;
 using Betonirai.WebApi.Models;
+using System.Text;
+using System.IO;
+using System.Web.Script.Serialization;
+using Recaptcha;
 
 namespace Betonirai.WebApi.Business
 {
@@ -49,6 +53,60 @@ namespace Betonirai.WebApi.Business
 
                 return responseString;
             }
+        }
+
+        public bool CheckCaptcha(CaptchaParametes requestModel)
+        {
+            Uri address = new Uri("http://www.google.com/recaptcha/api/verify");
+
+            // Create the web request  
+            HttpWebRequest request = WebRequest.Create(address) as HttpWebRequest;
+
+            // Set type to POST  
+            request.Method = "POST";
+            request.ContentType = "application/json";
+
+            object values = new { privatekey = PrivateKey, remoteip = requestModel.RemoteIP, challenge = requestModel.Challenge, response = requestModel.Response };
+            
+            // Create the data we want to send
+            string json = new JavaScriptSerializer().Serialize(values);
+
+            // Create a byte array of the data we want to send  
+            byte[] byteData = UTF8Encoding.UTF8.GetBytes(json);
+
+            // Set the content length in the request headers  
+            request.ContentLength = byteData.Length;
+
+            // Write data  
+            using (Stream postStream = request.GetRequestStream())
+            {
+                postStream.Write(byteData, 0, byteData.Length);
+            }
+
+            // Get response  
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                // Get the response stream  
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                // Console application output  
+                Console.WriteLine(reader.ReadToEnd());
+            }
+
+            return true;
+        }
+
+        public RecaptchaResponse Validate(CaptchaParametes requestModel)
+        {
+            RecaptchaValidator validator = new RecaptchaValidator();
+            validator.Challenge = requestModel.Challenge;
+            validator.PrivateKey = PrivateKey;
+            validator.RemoteIP = requestModel.RemoteIP;
+            validator.Response = requestModel.Response;
+
+            RecaptchaResponse response = validator.Validate();
+
+            return response;
         }
     }
 }
